ff4d9c70545708d7abcbf764204a55a48d07a435
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-01-15 12:30:42 +0100

    added compression algorithm

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const compress_images = require("compress-images")
+var INPUT_path_to_your_images
+var OUTPUT_path;
+
+INPUT_path_to_your_images = "dist/raw/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}";
+OUTPUT_path = "dist/compressed/";
+
+compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
+                { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+                { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+                { svg: { engine: "svgo", command: "--multipass" } },
+                { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+  function (error, completed, statistic) {
+    console.log("-------------");
+    console.log(error);
+    console.log(completed);
+    console.log(statistic);
+    console.log("-------------");
+  }
+);
